c45a73876e69617acc9ea0f9347ad068fab7e753
HexiOS1 <yassinedokali93@gmail.com>
2024-07-28 17:41:02 +0100

    Add files via upload

---
+++ file.js
@@ -0,0 +1,89 @@
+<script>
+
+function ActiveCodePatch(fpath, vaddr, bytes) {
+    if(!h5frida.ActiveCodePatch(fpath, vaddr, bytes)) {
+        var result = h5frida.ApplyCodePatch(fpath, vaddr, bytes);
+       
+    } return true;
+}
+function DeactiveCodePatch(fpath, vaddr, bytes) {
+    return h5frida.DeactiveCodePatch(fpath, vaddr, bytes);
+}
+
+ var codeStatus = false;
+
+ //function ở đây còn gà lắm nếu cần anh dạy cho cả làm html cho jb ko cần vá :)))
+ 
+        function AimCope() {
+        function
+		  	searchAndReplace(searchValue,
+        replaceValue,
+        type) {
+        h5gg.clearResults(); h5gg.searchNumber(searchValue,
+        type,
+        '0x100000000',
+        '0x160000000'); var results = h5gg.getResults(h5gg.getResultsCount()); for (var i = 0; i < results.length; i++) {
+        h5gg.setValue(results[i].address, replaceValue.toString(), type)}}
+        searchAndReplace(0.09, 180, 'F32')}
+
+					function aimfov() {
+		function searchAndReplace(searchValue, replaceValue, type) {
+        h5gg.clearResults(); h5gg.searchNumber(searchValue, type, '0x100000000', '0x160000000'); var results = h5gg.getResults(h5gg.getResultsCount()); for (var i = 0; i < results.length; i++) {
+        h5gg.setValue(results[i].address, replaceValue.toString(), type)}}
+        searchAndReplace(1053273620, -2000, 'I32')
+		searchAndReplace(1057048494, -2000, 'I32')
+        searchAndReplace(1054951342, -2000, 'I32')}
+			
+
+	 	function Norecoil() {
+        function searchAndReplace(searchValue, replaceValue, type) {
+        h5gg.clearResults(); h5gg.searchNumber(searchValue, type, '0x100000000', '0x160000000'); var results = h5gg.getResults(h5gg.getResultsCount()); for (var i = 0; i < results.length; i++) {
+        h5gg.setValue(results[i].address, replaceValue.toString(), type)}}
+        searchAndReplace(1016018816, 180, 'I32')}
+				
+function aimbot() {
+h5gg.clearResults();
+h5gg.searchNumber('31101603021', 'I64', '0x100000000', '0x160000000');
+var count = h5gg.getResultsCount();
+var r = h5gg.getResults(count);
+var cnt = 0;
+for(var i=0; i<count; i++) {
+    var addr1 = r[i].address;
+
+var dizhi0 = (Number(addr1) + 8);
+
+   
+
+
+{
+h5gg.setValue(dizhi0, 2139095040, "I32");
+
+   }
+}
+vant.showNotify({
+        type: 'success',
+        message: 'AWM自瞄'
+    });
+	}
+	function s1() {
+    h5gg.clearResults();
+    h5gg.searchNumber('72340173878198272', 'I64', '0x100000000', '0x130000000');
+    h5gg.editAll('72340172821233664', 'I64');
+    vant.showNotify({
+        type: 'success',
+        message: '快切'
+    });
+
+
+	}
+	
+function s1() {
+    modifyValue1(-0.05, Number(prompt("Nhập Độ Cao -0.05 -> ...  :", "")), 'F00', null,);
+}
+function s2() {
+    modifyValue1(2.5, Number(prompt("Nhập Tầm xa 2.5 -> ... :", "")), 'F04', null,);
+	}
+	
+	
+
+	</script>
